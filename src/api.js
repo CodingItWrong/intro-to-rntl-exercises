55a49b9d8c0f74792c6cf1eb5338ccdc5fde602d
Josh Justice <me@codingitwrong.com>
2022-10-22 16:27:27 +0100

    Fix typo

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'YOUR-API-KEY-YERE';
+const API_KEY = 'YOUR-API-KEY-HERE';
 
 const api = axios.create({
   baseURL: `https://api.outsidein.dev/${API_KEY}`,
